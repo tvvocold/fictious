b559026ecedb263a285a869b372a100fa8782084
GriffinMB <byatt.griffin@gmail.com>
2014-07-02 20:46:30 -0400

    Add basic subscription to backbone collections

--- app/assets/javascripts/views/collections/collection_show.js
+++ app/assets/javascripts/views/collections/collection_show.js
@@ -1,5 +1,14 @@
 FictiousApp.Views.CollectionShow = Backbone.View.extend({
+  initialize: function() {
+    this.view = this;
+  },
+
   template: JST["collections/show"],
+
+  events: {
+    'submit .follow-collection': 'subscribe',
+    'submit .unfollow-collection': 'unsubscribe'
+  },
 
   render: function() {
     var renderedContent = this.template({
@@ -8,5 +17,43 @@ FictiousApp.Views.CollectionShow = Backbone.View.extend({
 
     this.$el.html(renderedContent);
     return this;
+  },
+
+  subscribe: function(event) {
+    event.preventDefault();
+    var view = this.view
+    var $form = $('.follow-collection');
+    var formData = $('.follow-collection').serialize();
+
+    $.ajax({
+      type: "POST",
+      url: $form.attr("action"),
+      data: formData,
+      success: function(data){
+        debugger
+        view.render();
+      }
+    });
+  },
+
+  unsubscribe: function(event) {
+    event.preventDefault();
+    var view = this.view
+    var $form = $('.unfollow-collection');
+    var formData = $form.serialize();
+
+    $.ajax({
+      type: 'DELETE',
+      url: $form.attr("action"),
+      data: formData,
+      success: function(data) {
+        debugger
+        view.render();
+      }
+    });
+  },
+
+  swapButtons: function() {
+
   }
 });
--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -3,4 +3,33 @@ class Api::SubscriptionsController < ApplicationController
     @subscriptions = (current_user.subscription_posts + current_user.collection_sub_posts).uniq
     render :json => @subscriptions
   end
+
+  def create
+    if subscription_params[:user_id]
+      @subscription = Subscription.new(user_id: subscription_params[:user_id])
+    else
+      @subscription = Subscription.new(collection_id: subscription_params[:collection_id])
+    end
+    @subscription.subscriber_id = current_user.id
+    if @subscription.save
+      message = current_user.username + " subscribed to your posts!"
+      @subscription.notifications.create(content: message, user_id: subscription_params[:user_id])
+      render :json => @subscription
+    else
+      flash.now[:errors] = @subscription.errors.full_messages
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    @subscription = Subscription.find(params[:id])
+    @subscription.destroy()
+    render :json => @subscription
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:user_id, :collection_id)
+  end
 end
