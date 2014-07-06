ee7f154b6529ee18abd4083fea167fde04ad1e39
GriffinMB <byatt.griffin@gmail.com>
2014-07-06 01:23:04 -0400

    Add post to collection via collection

--- app/assets/javascripts/views/collections/collection_show.js
+++ app/assets/javascripts/views/collections/collection_show.js
@@ -1,14 +1,15 @@
 FictiousApp.Views.CollectionShow = Backbone.View.extend({
   initialize: function(options) {
     this.view = this;
-    this.listenTo(FictiousApp.collectionFeeds, 'add', this.render);
+    this.listenTo(FictiousApp.collectionFeeds, 'add', this.appendPost);
     this.listenTo(FictiousApp.subscriptions, 'add remove', this.render);
-    this.listenTo(FictiousApp.collections, 'all', this.render);
+    this.listenTo(FictiousApp.collections, 'add change', this.render);
   },
 
   template: JST["collections/show"],
 
   events: {
+    'submit .add-to-collection': 'addToCollection',
     'submit .follow-collection': 'subscribe',
     'submit .unfollow-collection': 'unsubscribe'
   },
@@ -17,13 +18,19 @@ FictiousApp.Views.CollectionShow = Backbone.View.extend({
     var colFeeds = FictiousApp.collectionFeeds.where({ collection_id: this.model.id });
     var postIds = [];
     var posts = [];
+    if (this.model.get('owner_id') === FictiousApp.currentUser) {
+      var userPosts = FictiousApp.posts.where({ author_id: FictiousApp.currentUser });
+    } else {
+      var userPosts = [];
+    }
 
     _(colFeeds).map(function(colFeed) { postIds.push(colFeed.get('post_id')) });
     _(postIds).map(function(postId) { posts.push(FictiousApp.posts.get(postId)) });
 
     var renderedContent = this.template({
       collection: this.model,
-      posts: posts
+      posts: posts,
+      userPosts: userPosts
     });
 
     this.$el.html(renderedContent);
@@ -67,6 +74,41 @@ FictiousApp.Views.CollectionShow = Backbone.View.extend({
         $('.new-post-button').attr('value', 'Follow Collection');
       }
     });
+  },
+
+  addToCollection: function(event) {
+    event.preventDefault();
+
+    var $form = $('.add-to-collection');
+    var formData = $(event.currentTarget).serialize();
+    $.ajax({
+      type: "POST",
+      url: $form.attr("action"),
+      data: formData,
+      success: function(data) {
+        console.log(data)
+        FictiousApp.collectionFeeds.add(new FictiousApp.Models.CollectionFeed(data));
+
+      },
+    });
+  },
+
+  appendPost: function() {
+    var colFeed = FictiousApp.collectionFeeds.last();
+    var colPost = FictiousApp.posts.get(colFeed.get('post_id'));
+    var user = FictiousApp.users.get(colPost.get('author_id'))
+    var colId = colPost.id;
+    var title = colPost.get('title');
+    var subtitle = colPost.get('subtitle');
+    var profilePic = user.get('profile_picture_url_thumb');
+    var userName = user.get('username');
+
+    $('.collection-posts').append('<li class="post-list collection-post-list"> \
+    <a href="#/posts/' + colId + '"> \
+    <h2>' + title + '</h2> \
+    <h4>' + subtitle + '</h4> \
+    <img src="' + profilePic + '"> <h5>' + userName + '</h5> \
+    </a></li>')
   },
 
   addCollectionPost: function() {
